1b022defce34725722366260d4a713e6d6b5a99a
vatiza <rakibur.mac@gmail.com>
2024-04-07 02:40:36 +0600

    login page desing done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login</h1>
-        </div>
+        <Container className='w-25 mx-auto'>
+            <h2>Plase Login</h2>
+             <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name='email' placeholder="Enter email" />
+       
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name='password' placeholder="Password" />
+      </Form.Group>
+     
+     
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      <br></br>
+      <Form.Text className="text-secendary">
+      Dont’t Have An Account ? 
+         <Link to='/register'>Register</Link>
+         </Form.Text>
+      <Form.Text className="text-success">
+         
+        </Form.Text>
+      <Form.Text className="text-danger">
+         
+        </Form.Text>
+    </Form>
+        </Container>
     );
 };
 
