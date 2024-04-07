915a9a511bbf01a4d5946a8619e336f106640516
vatiza <rakibur.mac@gmail.com>
2024-04-08 01:23:16 +0600

    register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,43 +1,40 @@
-import React from 'react';
-import {Container} from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <Container className='w-25 mx-auto'>
-            <h2>Plase Login</h2>
-             <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name='email' placeholder="Enter email" />
-       
-      </Form.Group>
+  return (
+    <Container className="w-25 mx-auto">
+      <h2>Plase Login</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name="email" placeholder="Enter email" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name='password' placeholder="Password" />
-      </Form.Group>
-     
-     
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <br></br>
-      <Form.Text className="text-secendary">
-      Dont’t Have An Account ? 
-         <Link to='/register'>Register</Link>
-         </Form.Text>
-      <Form.Text className="text-success">
-         
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <br></br>
+        <Form.Text className="text-secendary">
+          Dont’t Have An Account ?<Link to="/register">Register</Link>
         </Form.Text>
-      <Form.Text className="text-danger">
-         
-        </Form.Text>
-    </Form>
-        </Container>
-    );
+        <Form.Text className="text-success"></Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
 
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 const Register = () => {
     return (
-        <div>
-            <h1>THis is register page</h1>
-        </div>
+        <Container className="w-25 mx-auto">
+        <h2>Plase Register</h2>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control type="text" name="name" placeholder="Enter Full Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Photo Url</Form.Label>
+            <Form.Control type="text" name="photoUrl" placeholder="Enter Photo URl" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" name="email" placeholder="Enter email" />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+  
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <br></br>
+          <Form.Text className="text-secendary">
+            Already Have An Account ?<Link to="/login">Login</Link>
+          </Form.Text>
+          <Form.Text className="text-success"></Form.Text>
+          <Form.Text className="text-danger"></Form.Text>
+        </Form>
+      </Container>
     );
 };
 
