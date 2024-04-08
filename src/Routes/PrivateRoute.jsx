d26e5cc0c067488343583cbbbb044c85e427feba
vatiza <rakibur.mac@gmail.com>
2024-04-08 18:08:43 +0600

    check user already have login

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate } from "react-router";
 
-
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  if (user) {
+    return children;
+  }
+  return <Navigate to='/login'></Navigate>;
 };
 
 export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,49 +7,53 @@ import LoginLayout from "../Layout/LoginLayout";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Login/Register";
 import { Navigate } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 const router = createBrowserRouter([
-    {
-path:'/',
-element:<LoginLayout></LoginLayout>,
-children:[
-    {
-        path:'/',
-        element:<Navigate to="/category/0"></Navigate>
-        
-    },
-    {
-        path:'/login',
-        element:<Login></Login>
-    },
-    {
-        path:'/register',
-        element:<Register></Register>
-    }
-]
-    },
-    {
-        path: 'category',
-        element: <Main></Main>,
-        children: [
-            
-            {
-                path: ':id',
-                element: <Category></Category>,
-                loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
-            },
-           
-        ]
-    },
-    {
-        path: 'news',
-        element: <NewsLayout></NewsLayout>,
-        children: [
-            {
-                path: ':id',
-                element: <News></News>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <LoginLayout></LoginLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/category/0"></Navigate>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "category",
+    element: <Main></Main>,
+    children: [
+      {
+        path: ":id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/categories/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
+      {
+        path: ":id",
+        element: (
+          <PrivateRoute>
+            <News></News>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/news/${params.id}`),
+      },
+    ],
+  },
 ]);
 export default router;
